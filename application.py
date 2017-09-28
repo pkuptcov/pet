3ca1823fe83a5e4c546c08b1558480efe41575aa
pkuptcov <pavel.kuptcov@gmail.com>
2017-09-28 08:39:14 +0300

    v1.00

--- application.py
+++ application.py
@@ -1,7 +1,9 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 import time
 
+
 class Application:
+
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(10)
