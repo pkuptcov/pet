03f4c9733711194d892e3b382287a51d783f4cad
pkuptcov <pavel.kuptcov@gmail.com>
2017-09-28 16:50:38 +0300

    v1.00

--- application.py
+++ application.py
@@ -19,8 +19,8 @@ class Application:
         # Страница спасибо за покупку и переход в личный кабинет
         wd.find_element_by_css_selector("a.thanks__lk-link").click()
 
-    def create_order(self):
-        # Страница оформления заказа
+    def create_order_fiz_delivery(self):
+        # Страница оформления заказа для физю лиц доставка
         wd = self.wd
         wd.find_element_by_css_selector("[ng-model=\"orderDeliveryCtrl.order.deliveryAddress\"]").send_keys(
             "Россия, Санкт-Петербург, Благодатная улица, 6")
@@ -41,15 +41,81 @@ class Application:
             "тест")
         wd.find_element_by_css_selector("input[ng-click=\"orderDeliveryCtrl.make($event)\"]").click()
 
-    def init_order_creation(self):
+    def create_order_fiz_self(self):
+        wd = self.wd
+        wd.find_element_by_name("base").click()
+        wd.find_element_by_css_selector("input[value=\"online\"]").click()
+        wd.find_element_by_css_selector("[ng-model=\"orderingSelfCtrl.order.userEmail\"]").clear()
+        wd.find_element_by_css_selector("[ng-model=\"orderingSelfCtrl.order.userEmail\"]").send_keys(
+            "propetrovich@mail.ru")
+        wd.find_element_by_css_selector("[ng-model=\"orderingSelfCtrl.order.userPhone\"]").clear()
+        wd.find_element_by_css_selector("[ng-model=\"orderingSelfCtrl.order.userPhone\"]").send_keys(
+            "(111) 111-11-11")
+        wd.find_element_by_name("user_name").clear()
+        wd.find_element_by_name("user_name").send_keys("Тест")
+        wd.find_element_by_css_selector("textarea[ng-model='orderingSelfCtrl.order.userComment']").send_keys(
+            "тест")
+        wd.find_element_by_css_selector("input[ng-click=\"orderingSelfCtrl.make($event)\"]").click()
+
+    def create_order_ur_delivery(self):
+        wd = self.wd
+        wd.find_element_by_css_selector("[ng-model=\"orderDeliveryCtrl.order.deliveryAddress\"]").send_keys(
+            "Россия, Санкт-Петербург, Благодатная улица, 6")
+        wd.find_element_by_xpath("(//input[@name='delivery_day'])[3]").click()
+        wd.find_element_by_css_selector("[ng-change=\"orderDeliveryCtrl.deliveryTypeChange('standard')\"]").click()
+        time.sleep(2)
+        wd.find_element_by_css_selector("option[value='С2330До0330']").click()
+        wd.find_element_by_css_selector("input[placeholder=\"Название\"]").clear()
+        wd.find_element_by_css_selector("input[placeholder=\"Название\"]").send_keys("Тест")
+        wd.find_element_by_css_selector("input[placeholder=\"ИНН\"]").clear()
+        wd.find_element_by_css_selector("input[placeholder=\"ИНН\"]").send_keys("1231231231")
+        wd.find_element_by_css_selector("input[placeholder=\"КПП\"]").clear()
+        wd.find_element_by_css_selector("input[placeholder=\"КПП\"]").send_keys("123123123")
+        wd.find_element_by_css_selector("input[value=\"legalNonCash\"]").click()
+        wd.find_element_by_css_selector("[ng-model=\"orderDeliveryCtrl.order.userEmail\"]").clear()
+        wd.find_element_by_css_selector("[ng-model=\"orderDeliveryCtrl.order.userEmail\"]").send_keys(
+            "propetrovich@mail.ru")
+        wd.find_element_by_css_selector("[ng-model=\"orderDeliveryCtrl.order.userPhone\"]").clear()
+        wd.find_element_by_css_selector("[ng-model=\"orderDeliveryCtrl.order.userPhone\"]").send_keys(
+            "(111) 111-11-11")
+        wd.find_element_by_name("user_name").clear()
+        wd.find_element_by_name("user_name").send_keys("Тест")
+        wd.find_element_by_css_selector("textarea[ng-model='orderDeliveryCtrl.order.userComment']").send_keys(
+            "тест")
+        wd.find_element_by_css_selector("input[ng-click=\"orderDeliveryCtrl.make($event)\"]").click()
+
+    def create_order_ur_self(self):
+        wd = self.wd
+        wd.find_element_by_name("base").click()
+        wd.find_element_by_css_selector("input[placeholder=\"Название\"]").send_keys("Тест")
+        wd.find_element_by_css_selector("input[placeholder=\"ИНН\"]").clear()
+        wd.find_element_by_css_selector("input[placeholder=\"ИНН\"]").send_keys("1231231231")
+        wd.find_element_by_css_selector("input[placeholder=\"КПП\"]").clear()
+        wd.find_element_by_css_selector("input[placeholder=\"КПП\"]").send_keys("123123123")
+        wd.find_element_by_css_selector("input[value=\"legalNonCash\"]").click()
+        wd.find_element_by_css_selector("[ng-model=\"orderingSelfCtrl.order.userEmail\"]").clear()
+        wd.find_element_by_css_selector("[ng-model=\"orderingSelfCtrl.order.userEmail\"]").send_keys(
+            "propetrovich@mail.ru")
+        wd.find_element_by_css_selector("[ng-model=\"orderingSelfCtrl.order.userPhone\"]").clear()
+        wd.find_element_by_css_selector("[ng-model=\"orderingSelfCtrl.order.userPhone\"]").send_keys(
+            "(111) 111-11-11")
+        wd.find_element_by_name("user_name").clear()
+        wd.find_element_by_name("user_name").send_keys("Тест")
+        wd.find_element_by_css_selector("textarea[ng-model='orderingSelfCtrl.order.userComment']").send_keys(
+            "тест")
+        wd.find_element_by_css_selector("input[ng-click=\"orderingSelfCtrl.make($event)\"]").click()
+
+    def init_order_creation_delivery(self):
         # Нажимаем оформить
         wd = self.wd
+        wd.find_element_by_css_selector("input[value=delivery]").click()
         wd.find_element_by_css_selector("button[ng-click='totalCtrl.goToOrdering()']").click()
 
     def edit_card(self):
         # Удаляем предыдущую карту и вводим новую
         wd = self.wd
-        wd.find_element_by_link_text("Удалить").click()
+        if wd.find_elements_by_link_text("Удалить").is_present:
+            wd.find_element_by_link_text("Удалить").click()
         wd.find_element_by_css_selector("input[placeholder=\"•••••••\"]").send_keys("111111")
         wd.find_element_by_css_selector("button[ng-click='totalCtrl.addCard()']").click()
 
@@ -83,6 +149,18 @@ class Application:
         # Переход на сайт
         wd = self.wd
         wd.get("https://petrovich.ru/")
+
+    def init_order_creation_self(self):
+        # Выбираем самовывоз и нажимаем оформить
+        wd = self.wd
+        wd.find_element_by_css_selector("input[value=self]").click()
+        wd.find_element_by_css_selector("button[ng-click='totalCtrl.goToOrdering()']").click()
+        #time.sleep(1)
+
+    def select_ur(self):
+        wd = self.wd
+        # Выбор вкладки юридического лица
+        wd.find_element_by_xpath("//a[contains(text(),'Юридическое лицо')]").click()
 
     def destroy(self):
         self.wd.quit()
--- test_regress.py
+++ test_regress.py
@@ -10,14 +10,97 @@ def app(request):
     return fixture
 
 
-def fiz_delivery_auth(app):
+def test_fiz_delivery_auth(app):
     app.open_home_page()
     app.login(username="xigekuba@p33.org", password="111111")
     app.search()
     app.edit_quantity_product()
     app.go_to_cart()
     app.edit_card()
-    app.init_order_creation()
-    app.create_order()
+    app.init_order_creation_delivery()
+    app.create_order_fiz_delivery()
     app.thanks()
     app.logout()
+
+
+def test_fiz_delivery_non_auth(app):
+    app.open_home_page()
+    app.search()
+    app.edit_quantity_product()
+    app.go_to_cart()
+    app.edit_card()
+    app.init_order_creation_delivery()
+    app.create_order_fiz_delivery()
+    app.thanks()
+
+
+def test_fiz_self_auth(app):
+    app.open_home_page()
+    app.login(username="xigekuba@p33.org", password="111111")
+    app.search()
+    app.edit_quantity_product()
+    app.go_to_cart()
+    app.edit_card()
+    app.init_order_creation_self()
+    app.create_order_fiz_self()
+    app.thanks()
+    app.logout()
+
+
+def test_fiz_self_non_auth(app):
+    app.open_home_page()
+    app.search()
+    app.edit_quantity_product()
+    app.go_to_cart()
+    app.edit_card()
+    app.init_order_creation_self()
+    app.create_order_fiz_self()
+    app.thanks()
+
+
+def test_ur_delivery_auth(app):
+    app.open_home_page()
+    app.login(username="xigekuba@p33.org", password="111111")
+    app.search()
+    app.edit_quantity_product()
+    app.go_to_cart()
+    app.select_ur()
+    app.init_order_creation_delivery()
+    app.create_order_ur_delivery()
+    app.thanks()
+    app.logout()
+
+
+def test_ur_delivery_non_auth(app):
+    app.open_home_page()
+    app.search()
+    app.edit_quantity_product()
+    app.go_to_cart()
+    app.select_ur()
+    app.init_order_creation_delivery()
+    app.create_order_ur_delivery()
+    app.thanks()
+
+
+def test_ur_self_auth(app):
+    app.open_home_page()
+    app.login(username="xigekuba@p33.org", password="111111")
+    app.search()
+    app.edit_quantity_product()
+    app.go_to_cart()
+    app.select_ur()
+    app.init_order_creation_self()
+    app.create_order_ur_self()
+    app.thanks()
+    app.logout()
+
+
+def test_ur_self_non_auth(app):
+    app.open_home_page()
+    app.search()
+    app.edit_quantity_product()
+    app.go_to_cart()
+    app.select_ur()
+    app.init_order_creation_self()
+    app.create_order_ur_self()
+    app.thanks()
