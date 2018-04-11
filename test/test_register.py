ecf92ecc426f87c11949b540b75484d4228563cd
pkuptcov <pavel.kuptcov@gmail.com>
2018-04-11 13:53:41 +0300

    update register mail domain

--- test/test_register.py
+++ test/test_register.py
@@ -6,6 +6,6 @@ import time
 def test_register_gen(app):
     app.open_home_page()
     for i in range(10):
-        app.register.register(firstname="test",email=(''.join(choice(ascii_letters) for i in range(14))) + "@test.test", password="111111")
+        app.register.register(firstname="test",email=(''.join(choice(ascii_letters) for i in range(14))) + "@kluatr.ru", password="111111")
         #time.sleep(1)
         app.session.logout()
