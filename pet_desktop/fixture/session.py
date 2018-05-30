50009fcbeb0b1f84ace463fecd94a349cb9968a5
pkuptcov <pavel.kuptcov@gmail.com>
2018-05-30 09:51:54 +0300

    добавил ожидания

--- pet_desktop/fixture/session.py
+++ pet_desktop/fixture/session.py
@@ -1,3 +1,6 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
@@ -9,15 +12,17 @@ class SessionHelper:
     def login(self, username, password):
         # Авторизация
         wd = self.app.wd
+        wait = WebDriverWait(wd, 10)
         wd.find_element_by_link_text("Вход").click()
         wd.find_element_by_id("mainPetrovichLogin_login").send_keys(username)
         wd.find_element_by_id("mainPetrovichLogin_password").send_keys(password)
         wd.find_element_by_css_selector("div.form_row [type=submit]").click()
-        time.sleep(1)
+        wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "auth_user_link")))
 
     def logout(self):
         # Выход из ЛК
         wd = self.app.wd
+        wait = WebDriverWait(wd, 10)
         wd.find_element_by_css_selector("a.auth_user_link").click()
-        time.sleep(1)
+        wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Выход")))
         wd.find_element_by_link_text("Выход").click()
