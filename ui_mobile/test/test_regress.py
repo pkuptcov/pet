710b03dab40bb6a440eafce05f1f5716ad5d9535
pkuptcov <pavel.kuptcov@gmail.com>
2018-06-21 17:03:41 +0300

    fixes for chrome

--- ui_mobile/test/test_regress.py
+++ ui_mobile/test/test_regress.py
@@ -29,17 +29,17 @@
 #     app.regress.thanks()
 #
 #
-def test_fiz_self_auth(app):
-    app.open_home_page()
-    app.session.login(username="xigekuba@p33.org", password="111111")
-    app.regress.search()
-    app.regress.edit_quantity_product()
-    app.regress.go_to_cart()
-    app.regress.edit_card()
-    app.regress.init_order_creation_self()
-    app.regress.create_order_fiz_self()
-    app.regress.thanks()
-    app.session.logout()
+# def test_fiz_self_auth(app):
+#     app.open_home_page()
+#     app.session.login(username="xigekuba@p33.org", password="111111")
+#     app.regress.search()
+#     app.regress.edit_quantity_product()
+#     app.regress.go_to_cart()
+#     app.regress.edit_card()
+#     app.regress.init_order_creation_self()
+#     app.regress.create_order_fiz_self()
+#     app.regress.thanks()
+#     app.session.logout()
 #
 #
 # def test_fiz_self_non_auth(app):
@@ -77,17 +77,17 @@ def test_fiz_self_auth(app):
 #     app.regress.thanks()
 #
 #
-# def test_ur_self_auth(app):
-#     app.open_home_page()
-#     app.session.login(username="xigekuba@p33.org", password="111111")
-#     app.regress.search()
-#     app.regress.edit_quantity_product()
-#     app.regress.go_to_cart()
-#     app.regress.select_ur()
-#     app.regress.init_order_creation_self()
-#     app.regress.create_order_ur_self()
-#     app.regress.thanks()
-#     app.session.logout()
+def test_ur_self_auth(app):
+    app.open_home_page()
+    app.session.login(username="xigekuba@p33.org", password="111111")
+    app.regress.search()
+    app.regress.edit_quantity_product()
+    app.regress.go_to_cart()
+    app.regress.select_ur()
+    app.regress.init_order_creation_self()
+    app.regress.create_order_ur_self()
+    app.regress.thanks()
+    app.session.logout()
 #
 #
 # def test_ur_self_non_auth(app):
