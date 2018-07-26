7f0bb025fb1a2817a54c00bc7f35c44c9efe02e7
pkuptcov <pavel.kuptcov@gmail.com>
2018-07-26 18:34:08 +0300

    update

---
+++ ui_mobile/api-register.py
@@ -0,0 +1,29 @@
+import requests
+import random
+import string
+
+
+def api_registration():
+    username = ''.join(random.choices(string.ascii_lowercase + string.digits, k=20))
+    email = ("email={}@kluatr.ru".format(username))
+    payload = "firstName=test" \
+              "&lastName=testov" \
+              "&{}" \
+              "&password=111111" \
+              "&cardNumber=1111111" \
+              "&userMainRegisterForm=register" \
+              "&formName=userMainRegister".format(str(email))
+
+    url = "http://petrovich.efremov.dev.local/api/form"
+
+    headers = {
+        'content-type': "application/x-www-form-urlencoded",
+        'cache-control': "no-cache",
+        }
+
+    response = requests.request("POST", url, data=payload, headers=headers)
+
+    print(response.text)
+
+
+api_registration()
