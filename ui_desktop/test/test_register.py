6d9cc4f321216d727cef1a6d1d9ce8581b16872f
pkuptcov <pavel.kuptcov@gmail.com>
2018-07-30 10:30:03 +0300

    refactor email generation

--- ui_desktop/test/test_register.py
+++ ui_desktop/test/test_register.py
@@ -1,11 +1,11 @@
-from random import choice
-from string import ascii_letters
+import random
+import string
 
 
 def test_register_gen(app):
     app.open_home_page()
     for i in range(10):
         app.register.register(firstname="test",
-                              email=(''.join(choice(ascii_letters) for i in range(14))) + "@kluatr.ru",
+                              email=''.join(random.choices(string.ascii_lowercase + string.digits, k=20)) + "@kluatr.ru",
                               password="111111")
         app.session.logout()
